1ef048be83a830694fae9270a7782de11d03d8c9
hernan <hernanzazzarini@gmail.com>
2026-03-02 13:08:32 -0300

    config render ,deb y home ok

--- backend/complejo_futbol/complejo_futbol/urls.py
+++ backend/complejo_futbol/complejo_futbol/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Backend Complejo Futbol funcionando OK")
 
 urlpatterns = [
+    path('', home),   # 👈 ESTA ES LA QUE TE FALTA
     path('admin/', admin.site.urls),
     path('api/', include('reservas.urls')),
     path('api/auth/login/', TokenObtainPairView.as_view()),
